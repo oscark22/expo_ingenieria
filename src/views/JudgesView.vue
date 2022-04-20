d2d5b8505c29222cf8f46dea83e391abdfc31c03
voltageDifference <98506040+voltageDifference@users.noreply.github.com>
2022-04-19 21:39:00 -0700

    adding part of the form of judges

--- src/views/JudgesView.vue
+++ src/views/JudgesView.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="container">
+  <div class="container py-4 my-5">
+    <div class="row">
+      <div class="col">
+        <h1 class="fw-light">
+          Evaluación de proyectos
+        </h1>
+        <p class="lead text-muted">
+          Este apartado está dedicado a la evaluación de todos los proyectos de la Expo Ingeniería. Todos los proyectos, independientemente de cuál sea su categoría, se encuentran aquí.
+        </p>
+        <br>
+        <p class="lead">
+          Para evaluar un proyecto, selecciona la categoría a la que pertenece; sólo aquellos que pertenezcan a esa categoría podrán ser elegidos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet expedita accusamus magnam? Expedita ea error sint dolores reiciendis ad, velit necessitatibus, suscipit fugit sapiente nulla! Commodi minus nihil dolores aspernatur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni praesentium modi distinctio iusto eaque quis obcaecati pariatur ab vero incidunt illum, aliquam quaerat a ipsa reprehenderit est ipsam, soluta totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolor fuga velit facere omnis, accusantium quae perspiciatis reiciendis quasi illo molestiae assumenda quidem at iusto repellendus! Sequi porro aliquid dolor?
+        </p>
+      </div>
+    </div>
   </div>
   <div class="container">
+    <form>
+      <div class="row">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Selecciona una categoría</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+      <div class="row mt-3">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Elige un proyecto</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+    </form>
   </div>
 </template>
 
