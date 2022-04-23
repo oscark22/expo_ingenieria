0928deda5e84cbf5661f8c7219c8219567347f34
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-04-22 20:13:55 -0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,27 +5,6 @@
     </div>
   </div>
   <div id="contenedor-2">
-  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="@/assets/a.jpg" class="d-block w-100" alt="a">
-      </div>
-      <div class="carousel-item">
-        <img src="@/assets/b.jpg" class="d-block w-100" alt="b">
-      </div>
-      <div class="carousel-item">
-        <img src="@/assets/c.jpg" class="d-block w-100" alt="c">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
   <div class="container">
   <div class="container mt-5">
     <div class="row flex-lg-row-reverse p-4 gx-5 align-items-center rounded border shadow">
