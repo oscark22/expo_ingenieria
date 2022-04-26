bfeb274b2e332acb8743232150faf19b3975981d
voltageDifference <98506040+voltageDifference@users.noreply.github.com>
2022-04-25 20:31:52 -0700

    Smart buttons

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container text-center py-5 my-5">
-    <div class="row">
+  <div class="container text-center pt-5 mt-5">
+    <div class="row mb-3">
       <div class="col col-md-6 mx-auto">
         <h1 class="display-5 fw-light">
           Proyectos
@@ -11,25 +11,46 @@
       </div>
     </div>
     <div class="btn-group" role="group" aria-label="Basic outlined example">
-      <button @click="fetchData(urls[0])" type="button" class="btn btn-primary">
+      <button @click="fetchData(urls[0]); activeBtn='btn1';" type="button" class="btn btn-primary">
         <i class="bi bi-journal-bookmark-fill"></i>
         Académico
       </button>
-      <button @click="fetchData(urls[1])" type="button" class="btn btn-secondary">
+      <button @click="fetchData(urls[1]); activeBtn='btn2';" type="button" class="btn btn-secondary">
         <i class="bi bi-cpu"></i>
         Software y multimedia
       </button>
-      <button @click="fetchData(urls[1])" type="button" class="btn btn-success">
+      <button @click="fetchData(urls[1]); activeBtn='btn3';" type="button" class="btn btn-success">
         <i class="bi bi-box-seam"></i>
         Producto
       </button>
-      <button @click="fetchData(urls[1])" type="button" class="btn btn-danger">
+      <button @click="fetchData(urls[1]); activeBtn='btn4';" type="button" class="btn btn-danger">
         <i class="bi bi-gear-wide-connected"></i>
         Proceso
       </button>
     </div>
   </div>
-  <br>
+  <div class="container text-center pb-5 mb-5 mt-2">
+    <template v-if="activeBtn==='btn2'">
+      <div class="btn-group" role="group" aria-label="Basic outlined example">
+        <button @click="fetchData(urls[0])" type="button" class="btn btn-warning">
+          Categoría I
+        </button>
+        <button @click="fetchData(urls[0])" type="button" class="btn btn-dark">
+          Categoría II
+        </button>
+      </div>
+    </template>
+    <template v-else-if="activeBtn==='btn4'">
+      <div class="btn-group" role="group" aria-label="Basic outlined example">
+        <button @click="fetchData(urls[1])" type="button" class="btn btn-warning">
+          Categoría I
+        </button>
+        <button @click="fetchData(urls[1])" type="button" class="btn btn-dark">
+          Categoría II
+        </button>
+      </div>
+    </template>
+  </div>
   <div class="container">
     <div class="row row-cols-4 gy-4">
       <template v-for="card in cards" :key="card.id">
@@ -56,7 +77,8 @@ export default {
       urls: [
         'https://expoingenieria.com/rest_api_expo/tablas/proyecto.php',
         'https://expoingenieria.com/rest_api_expo/tablas/proyecto.php?id=2'
-      ]
+      ],
+      activeBtn: ''
     }
   },
   components: {
