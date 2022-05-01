a05810420b7d8a4499df5806f2999b716162f179
voltageDifference <98506040+voltageDifference@users.noreply.github.com>
2022-05-01 12:16:03 -0700

    Cards with steroids

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -11,41 +11,41 @@
       </div>
     </div>
     <div class="btn-group" role="group" aria-label="Basic outlined example">
-      <button @click="fetchData(); activeBtn='btn1';" type="button" class="btn btn-primary">
+      <button @click="fetchData(); activeBtn='btn1'; currCategory='0'" type="button" class="btn btn-primary">
         <i class="bi bi-journal-bookmark-fill"></i>
         Académico
       </button>
-      <button @click="activeBtn='btn2';" type="button" class="btn btn-secondary">
+      <button @click="activeBtn='btn2'" type="button" class="btn btn-secondary">
         <i class="bi bi-cpu"></i>
         Software y multimedia
       </button>
-      <button @click="fetchData(); activeBtn='btn3';" type="button" class="btn btn-success">
+      <button @click="fetchData(); activeBtn='btn3'; currCategory='3'" type="button" class="btn btn-success">
         <i class="bi bi-box-seam"></i>
         Producto
       </button>
-      <button @click="activeBtn='btn4';" type="button" class="btn btn-danger">
+      <button @click="activeBtn='btn4'" type="button" class="btn btn-danger">
         <i class="bi bi-gear-wide-connected"></i>
         Proceso
       </button>
     </div>
   </div>
   <div class="container text-center mb-md-5 mb-5 pb-md-4 pb-3 mt-2">
-    <template v-if="activeBtn==='btn2'">
+    <template v-if="activeBtn === 'btn2'">
       <div class="btn-group" role="group" aria-label="Basic outlined example">
-        <button @click="fetchData()" type="button" class="btn btn-warning">
+        <button @click="fetchData(); currCategory='1'" type="button" class="btn btn-warning">
           Categoría I
         </button>
-        <button @click="fetchData()" type="button" class="btn btn-dark">
+        <button @click="fetchData(); currCategory='2'" type="button" class="btn btn-dark">
           Categoría II
         </button>
       </div>
     </template>
-    <template v-else-if="activeBtn==='btn4'">
+    <template v-else-if="activeBtn === 'btn4'">
       <div class="btn-group" role="group" aria-label="Basic outlined example">
-        <button @click="fetchData()" type="button" class="btn btn-warning">
+        <button @click="fetchData(); currCategory='4'" type="button" class="btn btn-warning">
           Categoría I
         </button>
-        <button @click="fetchData()" type="button" class="btn btn-dark">
+        <button @click="fetchData(); currCategory='5'" type="button" class="btn btn-dark">
           Categoría II
         </button>
       </div>
@@ -54,7 +54,7 @@
   <div class="container mb-4">
     <div class="row row-cols-md-4 gy-4">
       <template v-for="card in cards" :key="card.proyecto_id">
-        <div class="col">
+        <div v-if="card.proyecto_id === this.currCategory" class="col">
           <CardComp
             :card_id="card.proyecto_id"
             :img_src="card.img_src"
@@ -76,7 +76,8 @@ export default {
     return {
       cards: [],
       errorMsg: '',
-      activeBtn: ''
+      activeBtn: '',
+      currCategory: '0'
     }
   },
   components: {
