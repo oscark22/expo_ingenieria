6c1224eacb7cac7b7f0bc0f9b2884e54e21ba751
voltageDifference <98506040+voltageDifference@users.noreply.github.com>
2022-05-01 20:19:21 -0700

    footer changes

--- src/App.vue
+++ src/App.vue
@@ -58,19 +58,19 @@
     </div>
   </div>
   <router-view/>
-  <footer class="footer pt-4 pb-4 mt-5 mb-2">
-    <div class="container-fluid">
-      <div class="row gx-md-5 px-md-5 mx-md-5 justify-content-center">
-        <div class="col-3">
-          <img src="@/assets/logo_expo_anio.png" class="img-fluid" width="200" alt="logo_expo">
-        </div>
-        <div class="col-3">
-          <img src="@/assets/logo_tyva.png" class="img-fluid" width="200" alt="logo_tyva">
-        </div>
-        <div class="col-3">
+<footer class="footer mt-5 mb-2">
+    <div class="container">
+      <div class="row flex-row text-center p-4 p-md-0">
+        <div class="col col-6 col-md-3">
           <img src="@/assets/logo_ceid.png" class="img-fluid" width="200" alt="logo_ceid">
         </div>
-        <div class="col-3">
+        <div class="col col-6 col-md-3">
+          <img src="@/assets/logo_expo_anio.png" class="img-fluid" width="200" alt="logo_expo">
+        </div>
+        <div class="col col-6 col-md-3">
+          <img src="@/assets/logo_tyva.png" class="img-fluid" width="200" alt="logo_tyva">
+        </div>
+        <div class="col col-6 col-md-3">
           <img src="@/assets/logo_ciap.png" class="img-fluid" width="200" alt="logo_ciap">
         </div>
       </div>
