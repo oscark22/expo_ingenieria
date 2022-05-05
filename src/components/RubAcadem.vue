40ffacb7ceb51af353a4fb94dfb1f6599f5908a4
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-05-05 12:27:55 -0700

    Update RubAcadem.vue

--- src/components/RubAcadem.vue
+++ src/components/RubAcadem.vue
@@ -8,26 +8,19 @@
             <p>
                 <strong>Calificación explicación de contexto general</strong>
             </p>
-            <div class="col-auto">
-                <input type="number" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
-            </div>
-            <div class="col-auto">
-                <span id="passwordHelpInline" class="form-text">
-                Must be 8-20 characters long.
-                </span>
-            </div>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación explicación de concepto</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>No aplica</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -35,25 +28,19 @@
             <p>
                 <strong>Calificación presentación presencial (si aplica)</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>No aplica</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación aspectos audiovisuales de la presentación</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -61,25 +48,19 @@
             <p>
                 <strong>Calificación calidad de exposición verbal</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación de la participación</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -87,25 +68,19 @@
             <p>
                 <strong>Calificación de tiempo de la exposición</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación del desarrollo de sistemas propios</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -113,25 +88,19 @@
             <p>
                 <strong>Calificación de originalidad del proyecto</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación de la funcionalidad</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -139,25 +108,19 @@
             <p>
                 <strong>Calificación de relación entre concepto físico y explicación teórica</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación de tiempo de respuesta</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -165,25 +128,19 @@
             <p>
                 <strong>Calificación del concepto de la comunicación visual (si aplica)</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación del diseño del proyecto</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div class="row">
@@ -191,41 +148,42 @@
             <p>
                 <strong>Calificación de la organización de los elementos</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
         <div class="col-12 col-md-6">
             <p>
                 <strong>Calificación del diseño de la interfaz</strong>
             </p>
-            <select class="form-select mb-4" aria-label="Default select example">
-                <option selected>Calificación</option>
-                <option value="1">0-25</option>
-                <option value="2">26-50</option>
-                <option value="3">51-75</option>
-                <option value="4">76-100</option>
-            </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
         </div>
     </div>
     <div>
         <p>
             <strong>Calificación general</strong>
         </p>
-        <select class="form-select mb-4" aria-label="Default select example">
-        <option selected>Calificación</option>
-        <option value="1">0-25</option>
-        <option value="2">26-50</option>
-        <option value="3">51-75</option>
-        <option value="4">76-100</option>
-        </select>
+        <div>
+            <label for="gradeRange" class="form-label">Calificación: {{ currGrade }}</label>
+            <input v-model="currGrade" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+        </div>
     </div>
     <div class="form-floating">
         <textarea class="form-control" placeholder="Deje un comentario en este espacio" id="floatingTextarea" style="height: 100px"></textarea>
         <label for="floatingTextarea">Comentarios adicionales</label>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      currGrade: 0
+    }
+  }
+}
+</script>
