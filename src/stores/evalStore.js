54f7d812b8cd524f55cdeb5918fda64aff46ddd7
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-05-11 01:02:18 -0700

    6 rubricas hechas

--- src/stores/evalStore.js
+++ src/stores/evalStore.js
@@ -26,7 +26,7 @@ export const cardStore = defineStore ('evaluation', {
             params.append('calificacion_contextoGeneral', )
             params.append('calificacion_explicacionConcepto', )
             params.append('calificacion_presentacionFisica', )
-            params.append('calificacion_explicacionAudiovisual', )
+            params.append('calificacion_presentacionAudiovisual', )
             params.append('calificacion_ClaridadFluidez', )
             params.append('calificacion_participacion', )
             params.append('calificacion_tiempoExposicion', )
@@ -46,8 +46,8 @@ export const cardStore = defineStore ('evaluation', {
                 console.log(error)
             }
         },
-        async postAcademic () {
-            const url = 'https://expoingenieria.com/rest_api_expo/tablas/rubrica_proyectoacademico.php/'
+        async postProd1 () {
+            const url = 'https://expoingenieria.com/rest_api_expo/tablas/rubrica_c1_innovacionProcesoProducto.php/'
             const config = this.config
 
             const params = new URLSearchParams()
@@ -57,12 +57,15 @@ export const cardStore = defineStore ('evaluation', {
             params.append('observacion', )
             params.append('calificacion_contextoGeneral', )
             params.append('calificacion_explicacionConcepto', )
-            params.append('calificacion_presentacionFisica', )
-            params.append('calificacion_explicacionAudiovisual', )
+            params.append('calificacion_presentacionAudiovisual', )
             params.append('calificacion_ClaridadFluidez', )
             params.append('calificacion_participacion', )
             params.append('calificacion_tiempoExposicion', )
-            params.append('calificacion_desarrolloSistPropios', )
+            params.append('calificacion_relacionFisicoTeorica', )
+            params.append('calificacion_calidadNarrativaDistracciones', )
+            params.append('calificacion_calidadVideo', )
+            params.append('calificacion_originalidad', )
+            params.append('calificacion_funcionalidad', )
 
             try {
                 const response = await axios.post(url, params, config)
@@ -70,5 +73,126 @@ export const cardStore = defineStore ('evaluation', {
                 console.log(error)
             }
         }
+    },
+    async postIndustria () {
+        const url = 'https://expoingenieria.com/rest_api_expo/tablas/rubrica_aplicacionIndustria.php/'
+        const config = this.config
+
+        const params = new URLSearchParams()
+        params.append('proyecto_id', )
+        params.append('juez_id', )
+        params.append('calificacion', )
+        params.append('observacion', )
+        params.append('calificacion_contextoGeneral', )
+        params.append('calificacion_explicacionConcepto', )
+        params.append('calificacion_presentacionAudiovisual', )
+        params.append('calificacion_presentacionFisica', )
+        params.append('calificacion_ClaridadFluidez', )
+        params.append('calificacion_participacion', )
+        params.append('calificacion_tiempoExposicion', )
+        params.append('calificacion_calidadNarrativaDistracciones', )
+        params.append('calificacion_calidadVideo', )
+        params.append('calificacion_originalidad', )
+        params.append('calificacion_funcionalidad', )
+        params.append('calificacion_tiempoRespuesta', )
+        params.append('calificacion_desarrolloSistPropios', )
+
+        try {
+            const response = await axios.post(url, params, config)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async postSWM1 () {
+        const url = 'https://expoingenieria.com/rest_api_expo/tablas/rubrica_c1_innovacionProceso_SWMultimedia.php/'
+        const config = this.config
+
+        const params = new URLSearchParams()
+        params.append('proyecto_id', )
+        params.append('juez_id', )
+        params.append('calificacion', )
+        params.append('observacion', )
+        params.append('calificacion_contextoGeneral', )
+        params.append('calificacion_explicacionConcepto', )
+        params.append('calificacion_presentacionAudiovisual', )
+        params.append('calificacion_presentacionFisica', )
+        params.append('calificacion_ClaridadFluidez', )
+        params.append('calificacion_participacion', )
+        params.append('calificacion_tiempoExposicion', )
+        params.append('calificacion_calidadNarrativaDistracciones', )
+        params.append('calificacion_calidadVideo', )
+        params.append('calificacion_funcionalidad', )
+        params.append('calificacion_tiempoRespuesta', )
+        params.append('calificacion_relacionFisicoTeorica', )
+        params.append('calificacion_conceptoComunicacionVisualMensaje', )
+        params.append('calificacion_disenoInterfaz', )
+
+        try {
+            const response = await axios.post(url, params, config)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async postSWM2 () {
+        const url = 'https://expoingenieria.com/rest_api_expo/tablas/rubrica_c2_innovacionProceso_SWMultimedia.php/'
+        const config = this.config
+
+        const params = new URLSearchParams()
+        params.append('proyecto_id', )
+        params.append('juez_id', )
+        params.append('calificacion', )
+        params.append('observacion', )
+        params.append('calificacion_contextoGeneral', )
+        params.append('calificacion_explicacionConcepto', )
+        params.append('calificacion_presentacionFisica', )
+        params.append('calificacion_presentacionAudiovisual', )
+        params.append('calificacion_ClaridadFluidez', )
+        params.append('calificacion_participacion', )
+        params.append('calificacion_tiempoExposicion', )
+        params.append('calificacion_desarrolloSistPropios', )
+        params.append('calificacionOriginalidad', )
+        params.append('calificacion_funcionalidad', )
+        params.append('calificacion_conceptoComunicacionVisualMensaje', )
+        params.append('calificacion_diseno', )
+        params.append('calificacion_organizacionElementos', )
+        params.append('calificacion_disenoInterfaz', )
+        params.append('calificacion_calidadNarrativaDistracciones', )
+        params.append('calificacion_calidadVideo', )
+
+        try {
+            const response = await axios.post(url, params, config)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async postProd2 () {
+        const url = 'https://expoingenieria.com/rest_api_expo/tablas/rubrica_c2_innovacionProcesoProducto.php/'
+        const config = this.config
+
+        const params = new URLSearchParams()
+        params.append('proyecto_id', )
+        params.append('juez_id', )
+        params.append('calificacion', )
+        params.append('observacion', )
+        params.append('calificacion_contextoGeneral', )
+        params.append('calificacion_explicacionConcepto', )
+        params.append('calificacion_presentacionAudiovisual', )
+        params.append('calificacion_presentacionFisica', )
+        params.append('calificacion_ClaridadFluidez', )
+        params.append('calificacion_participacion', )
+        params.append('calificacion_tiempoExposicion', )
+        params.append('calificacion_calidadNarrativaDistracciones', )
+        params.append('calificacion_calidadVideo', )
+        params.append('calificacion_originalidad', )
+        params.append('calificacion_funcionalidad', )
+        params.append('calificacion_tiempoRespuesta', )
+        params.append('calificacion_desarrolloSistPropios', )
+
+        try {
+            const response = await axios.post(url, params, config)
+        } catch (error) {
+            console.log(error)
+        }
     }
-})
+}
+)
