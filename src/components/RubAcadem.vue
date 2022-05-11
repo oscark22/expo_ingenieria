bd574668a81538ee08bf462f4a2945132339a564
IAmRamiro3 <ramironunezsanchez3@gmail.com>
2022-05-11 02:08:59 -0700

    cositas buenas

--- src/components/RubAcadem.vue
+++ src/components/RubAcadem.vue
@@ -10,8 +10,8 @@
                 <strong>Calificación explicación de contexto general</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[1] }}</label>
-            <input v-model="currGrade[1]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[2] }}</label>
+            <input v-model="grade.grades[2]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -19,19 +19,19 @@
                 <strong>Calificación explicación de concepto</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[2] }}</label>
-            <input v-model="currGrade[2]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[3] }}</label>
+            <input v-model="grade.grades[3]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
     <div class="row mb-3 mt-4">
         <div class="col-12 col-md-6">
             <p>
-                <strong>Calificación presentación presencial (si aplica)</strong>
+                <strong>Calificación presentación física</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[3] }}</label>
-            <input v-model="currGrade[3]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[4] }}</label>
+            <input v-model="grade.grades[4]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -39,8 +39,8 @@
                 <strong>Calificación aspectos audiovisuales de la presentación</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[4] }}</label>
-            <input v-model="currGrade[4]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[5] }}</label>
+            <input v-model="grade.grades[5]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -50,8 +50,8 @@
                 <strong>Calificación fluidez en la presentación</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[5] }}</label>
-            <input v-model="currGrade[5]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[6] }}</label>
+            <input v-model="grade.grades[6]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -59,8 +59,8 @@
                 <strong>Calificación de la participación</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[6] }}</label>
-            <input v-model="currGrade[6]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[7] }}</label>
+            <input v-model="grade.grades[7]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -70,8 +70,8 @@
                 <strong>Calificación de tiempo de la exposición</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[7] }}</label>
-            <input v-model="currGrade[7]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[8] }}</label>
+            <input v-model="grade.grades[8]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -79,8 +79,8 @@
                 <strong>Calificación del desarrollo de sistemas propios</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[8] }}</label>
-            <input v-model="currGrade[8]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[9] }}</label>
+            <input v-model="grade.grades[9]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -90,8 +90,8 @@
                 <strong>Calificación de originalidad del proyecto</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[9] }}</label>
-            <input v-model="currGrade[9]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[10] }}</label>
+            <input v-model="grade.grades[10]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -99,8 +99,8 @@
                 <strong>Calificación de la funcionalidad del proyecto</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[10] }}</label>
-            <input v-model="currGrade[10]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[11] }}</label>
+            <input v-model="grade.grades[11]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -110,8 +110,8 @@
                 <strong>Calificación de relación entre concepto físico y explicación teórica</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[11] }}</label>
-            <input v-model="currGrade[11]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[12] }}</label>
+            <input v-model="grade.grades[12]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -119,8 +119,8 @@
                 <strong>Calificación de tiempo de respuesta</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[12] }}</label>
-            <input v-model="currGrade[12]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[13] }}</label>
+            <input v-model="grade.grades[13]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -130,8 +130,8 @@
                 <strong>Calificación del concepto de la comunicación visual (si aplica)</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[13] }}</label>
-            <input v-model="currGrade[13]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[14] }}</label>
+            <input v-model="grade.grades[14]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -139,8 +139,8 @@
                 <strong>Calificación del diseño del proyecto</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[14] }}</label>
-            <input v-model="currGrade[14]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[15] }}</label>
+            <input v-model="grade.grades[15]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -150,8 +150,8 @@
                 <strong>Calificación de la organización de los elementos</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[15] }}</label>
-            <input v-model="currGrade[15]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[16] }}</label>
+            <input v-model="grade.grades[16]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
         <div class="col-12 col-md-6 mt-3 mt-md-0">
@@ -159,8 +159,8 @@
                 <strong>Calificación del diseño de la interfaz del proyecto</strong>
             </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[16] }}</label>
-            <input v-model="currGrade[16]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[17] }}</label>
+            <input v-model="grade.grades[17]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
         </div>
     </div>
@@ -169,8 +169,8 @@
             <strong>Calificación general</strong>
         </p>
         <div>
-            <label for="gradeRange" class="form-label">Calificación: {{ currGrade[17] }}</label>
-            <input v-model="currGrade[17]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
+            <label for="gradeRange" class="form-label">Calificación: {{ grade.grades[1] }}</label>
+            <input v-model="grade.grades[1]" type="range" class="form-range" min="0" max="100" step="1" id="gradeRange">
         </div>
     </div>
     <div class="form-floating">
@@ -179,31 +179,14 @@
     </div>
 </template>
 
+<script setup>
+
+import { evalStore } from '@/stores/evalStore'
+
+const grade = evalStore()
+
+</script>
+
 <script>
-export default {
-  data () {
-    return {
-      // prefered method
-      currGrade: {
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0,
-        9: 0,
-        10: 0,
-        11: 0,
-        12: 0,
-        13: 0,
-        14: 0,
-        15: 0,
-        16: 0,
-        17: 0
-      }
-    }
-  }
-}
+
 </script>
--- src/stores/evalStore.js
+++ src/stores/evalStore.js
@@ -3,7 +3,7 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
-export const cardStore = defineStore ('evaluation', {
+export const evalStore = defineStore ('evaluation', {
     state: () => {
         return {
             config: {
@@ -13,7 +13,27 @@ export const cardStore = defineStore ('evaluation', {
             },
 
             proyecto_id: '',
-            juez_id: localStorage.getItem('id')
+            juez_id: localStorage.getItem('id'),
+
+            grade: {
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0,
+                6: 0,
+                7: 0,
+                8: 0,
+                9: 0,
+                10: 0,
+                11: 0,
+                12: 0,
+                13: 0,
+                14: 0,
+                15: 0,
+                16: 0,
+                17: 0
+            }
         }
     },
     actions: {
@@ -24,24 +44,24 @@ export const cardStore = defineStore ('evaluation', {
             const params = new URLSearchParams()
             params.append('proyecto_id', )
             params.append('juez_id', )
-            params.append('calificacion', )
+            params.append('calificacion', grade[1])
             params.append('observacion', )
-            params.append('calificacion_contextoGeneral', )
-            params.append('calificacion_explicacionConcepto', )
-            params.append('calificacion_presentacionFisica', )
-            params.append('calificacion_presentacionAudiovisual', )
-            params.append('calificacion_ClaridadFluidez', )
-            params.append('calificacion_participacion', )
-            params.append('calificacion_tiempoExposicion', )
-            params.append('calificacion_desarrolloSistPropios', )
-            params.append('calificacion_originalidad', )
-            params.append('calificacion_funcionalidad', )
-            params.append('calificacion_relacionFisicoTeorica', )
-            params.append('calificacion_tiempoRespuesta', )
-            params.append('calificacion_conceptoComunicacionVisualMensaje', )
-            params.append('calificacion_diseno', )
-            params.append('calificacion_organizacionElementos', )
-            params.append('calificacion_disenoInterfaz', )
+            params.append('calificacion_contextoGeneral', grade[2])
+            params.append('calificacion_explicacionConcepto', grade[3])
+            params.append('calificacion_presentacionFisica', grade[4])
+            params.append('calificacion_presentacionAudiovisual', grade[5])
+            params.append('calificacion_ClaridadFluidez', grade[6])
+            params.append('calificacion_participacion', grade[7])
+            params.append('calificacion_tiempoExposicion', grade[8])
+            params.append('calificacion_desarrolloSistPropios', grade[9])
+            params.append('calificacion_originalidad', grade[10])
+            params.append('calificacion_funcionalidad', grade[11])
+            params.append('calificacion_relacionFisicoTeorica', grade[12])
+            params.append('calificacion_tiempoRespuesta', grade[13])
+            params.append('calificacion_conceptoComunicacionVisualMensaje', grade[14])
+            params.append('calificacion_diseno', grade[15])
+            params.append('calificacion_organizacionElementos', grade[16])
+            params.append('calificacion_disenoInterfaz', grade[17])
 
             try {
                 const response = await axios.post(url, params, config)
@@ -56,19 +76,19 @@ export const cardStore = defineStore ('evaluation', {
             const params = new URLSearchParams()
             params.append('proyecto_id', )
             params.append('juez_id', )
-            params.append('calificacion', )
+            params.append('calificacion', grade[1])
             params.append('observacion', )
-            params.append('calificacion_contextoGeneral', )
-            params.append('calificacion_explicacionConcepto', )
-            params.append('calificacion_presentacionAudiovisual', )
-            params.append('calificacion_ClaridadFluidez', )
-            params.append('calificacion_participacion', )
-            params.append('calificacion_tiempoExposicion', )
-            params.append('calificacion_relacionFisicoTeorica', )
-            params.append('calificacion_calidadNarrativaDistracciones', )
-            params.append('calificacion_calidadVideo', )
-            params.append('calificacion_originalidad', )
-            params.append('calificacion_funcionalidad', )
+            params.append('calificacion_contextoGeneral', grade[2])
+            params.append('calificacion_explicacionConcepto', grade[3])
+            params.append('calificacion_presentacionAudiovisual', grade[4])
+            params.append('calificacion_ClaridadFluidez', grade[5])
+            params.append('calificacion_participacion', grade[6])
+            params.append('calificacion_tiempoExposicion', grade[7])
+            params.append('calificacion_relacionFisicoTeorica', grade[8])
+            params.append('calificacion_calidadNarrativaDistracciones', grade[9])
+            params.append('calificacion_calidadVideo', grade[10])
+            params.append('calificacion_originalidad', grade[11])
+            params.append('calificacion_funcionalidad', grade[12])
 
             try {
                 const response = await axios.post(url, params, config)
@@ -84,21 +104,21 @@ export const cardStore = defineStore ('evaluation', {
         const params = new URLSearchParams()
         params.append('proyecto_id', )
         params.append('juez_id', )
-        params.append('calificacion', )
+        params.append('calificacion', grade[1])
         params.append('observacion', )
-        params.append('calificacion_contextoGeneral', )
-        params.append('calificacion_explicacionConcepto', )
-        params.append('calificacion_presentacionAudiovisual', )
-        params.append('calificacion_presentacionFisica', )
-        params.append('calificacion_ClaridadFluidez', )
-        params.append('calificacion_participacion', )
-        params.append('calificacion_tiempoExposicion', )
-        params.append('calificacion_calidadNarrativaDistracciones', )
-        params.append('calificacion_calidadVideo', )
-        params.append('calificacion_originalidad', )
-        params.append('calificacion_funcionalidad', )
-        params.append('calificacion_tiempoRespuesta', )
-        params.append('calificacion_desarrolloSistPropios', )
+        params.append('calificacion_contextoGeneral', grade[2])
+        params.append('calificacion_explicacionConcepto', grade[3])
+        params.append('calificacion_presentacionAudiovisual', grade[4])
+        params.append('calificacion_presentacionFisica', grade[5])
+        params.append('calificacion_ClaridadFluidez', grade[6])
+        params.append('calificacion_participacion', grade[7])
+        params.append('calificacion_tiempoExposicion', grade[8])
+        params.append('calificacion_calidadNarrativaDistracciones', grade[9])
+        params.append('calificacion_calidadVideo', grade[10])
+        params.append('calificacion_originalidad', grade[11])
+        params.append('calificacion_funcionalidad', grade[12])
+        params.append('calificacion_tiempoRespuesta', grade[13])
+        params.append('calificacion_desarrolloSistPropios', grade[14])
 
         try {
             const response = await axios.post(url, params, config)
@@ -113,22 +133,22 @@ export const cardStore = defineStore ('evaluation', {
         const params = new URLSearchParams()
         params.append('proyecto_id', )
         params.append('juez_id', )
-        params.append('calificacion', )
+        params.append('calificacion', grade[1])
         params.append('observacion', )
-        params.append('calificacion_contextoGeneral', )
-        params.append('calificacion_explicacionConcepto', )
-        params.append('calificacion_presentacionAudiovisual', )
-        params.append('calificacion_presentacionFisica', )
-        params.append('calificacion_ClaridadFluidez', )
-        params.append('calificacion_participacion', )
-        params.append('calificacion_tiempoExposicion', )
-        params.append('calificacion_calidadNarrativaDistracciones', )
-        params.append('calificacion_calidadVideo', )
-        params.append('calificacion_funcionalidad', )
-        params.append('calificacion_tiempoRespuesta', )
-        params.append('calificacion_relacionFisicoTeorica', )
-        params.append('calificacion_conceptoComunicacionVisualMensaje', )
-        params.append('calificacion_disenoInterfaz', )
+        params.append('calificacion_contextoGeneral', grade[2])
+        params.append('calificacion_explicacionConcepto', grade[3])
+        params.append('calificacion_presentacionAudiovisual', grade[4])
+        params.append('calificacion_presentacionFisica', grade[5])
+        params.append('calificacion_ClaridadFluidez', grade[6])
+        params.append('calificacion_participacion', grade[7])
+        params.append('calificacion_tiempoExposicion', grade[8])
+        params.append('calificacion_calidadNarrativaDistracciones', grade[9])
+        params.append('calificacion_calidadVideo', grade[10])
+        params.append('calificacion_funcionalidad', grade[11])
+        params.append('calificacion_tiempoRespuesta', grade[12])
+        params.append('calificacion_relacionFisicoTeorica', grade[13])
+        params.append('calificacion_conceptoComunicacionVisualMensaje', grade[14])
+        params.append('calificacion_disenoInterfaz', grade[15])
 
         try {
             const response = await axios.post(url, params, config)
@@ -143,24 +163,24 @@ export const cardStore = defineStore ('evaluation', {
         const params = new URLSearchParams()
         params.append('proyecto_id', )
         params.append('juez_id', )
-        params.append('calificacion', )
+        params.append('calificacion', grade[1])
         params.append('observacion', )
-        params.append('calificacion_contextoGeneral', )
-        params.append('calificacion_explicacionConcepto', )
-        params.append('calificacion_presentacionFisica', )
-        params.append('calificacion_presentacionAudiovisual', )
-        params.append('calificacion_ClaridadFluidez', )
-        params.append('calificacion_participacion', )
-        params.append('calificacion_tiempoExposicion', )
-        params.append('calificacion_desarrolloSistPropios', )
-        params.append('calificacionOriginalidad', )
-        params.append('calificacion_funcionalidad', )
-        params.append('calificacion_conceptoComunicacionVisualMensaje', )
-        params.append('calificacion_diseno', )
-        params.append('calificacion_organizacionElementos', )
-        params.append('calificacion_disenoInterfaz', )
-        params.append('calificacion_calidadNarrativaDistracciones', )
-        params.append('calificacion_calidadVideo', )
+        params.append('calificacion_contextoGeneral', grade[2])
+        params.append('calificacion_explicacionConcepto', grade[3])
+        params.append('calificacion_presentacionFisica', grade[4])
+        params.append('calificacion_presentacionAudiovisual', grade[5])
+        params.append('calificacion_ClaridadFluidez', grade[6])
+        params.append('calificacion_participacion', grade[7])
+        params.append('calificacion_tiempoExposicion', grade[8])
+        params.append('calificacion_desarrolloSistPropios', grade[9])
+        params.append('calificacionOriginalidad', grade[10])
+        params.append('calificacion_funcionalidad', grade[11])
+        params.append('calificacion_conceptoComunicacionVisualMensaje', grade[12])
+        params.append('calificacion_diseno', grade[13])
+        params.append('calificacion_organizacionElementos', grade[14])
+        params.append('calificacion_disenoInterfaz', grade[15])
+        params.append('calificacion_calidadNarrativaDistracciones', grade[16])
+        params.append('calificacion_calidadVideo', grade[17])
 
         try {
             const response = await axios.post(url, params, config)
@@ -175,21 +195,21 @@ export const cardStore = defineStore ('evaluation', {
         const params = new URLSearchParams()
         params.append('proyecto_id', )
         params.append('juez_id', )
-        params.append('calificacion', )
+        params.append('calificacion', grade[1])
         params.append('observacion', )
-        params.append('calificacion_contextoGeneral', )
-        params.append('calificacion_explicacionConcepto', )
-        params.append('calificacion_presentacionAudiovisual', )
-        params.append('calificacion_presentacionFisica', )
-        params.append('calificacion_ClaridadFluidez', )
-        params.append('calificacion_participacion', )
-        params.append('calificacion_tiempoExposicion', )
-        params.append('calificacion_calidadNarrativaDistracciones', )
-        params.append('calificacion_calidadVideo', )
-        params.append('calificacion_originalidad', )
-        params.append('calificacion_funcionalidad', )
-        params.append('calificacion_tiempoRespuesta', )
-        params.append('calificacion_desarrolloSistPropios', )
+        params.append('calificacion_contextoGeneral', grade[2])
+        params.append('calificacion_explicacionConcepto', grade[3])
+        params.append('calificacion_presentacionAudiovisual', grade[4])
+        params.append('calificacion_presentacionFisica', grade[5])
+        params.append('calificacion_ClaridadFluidez', grade[6])
+        params.append('calificacion_participacion', grade[7])
+        params.append('calificacion_tiempoExposicion', grade[8])
+        params.append('calificacion_calidadNarrativaDistracciones', grade[9])
+        params.append('calificacion_calidadVideo', grade[10])
+        params.append('calificacion_originalidad', grade[11])
+        params.append('calificacion_funcionalidad', grade[12])
+        params.append('calificacion_tiempoRespuesta', grade[13])
+        params.append('calificacion_desarrolloSistPropios', grade[14])
 
         try {
             const response = await axios.post(url, params, config)
